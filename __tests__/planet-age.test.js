62feb84522ed68a4d712b00b1f2ef8c823d95764
tkm404 <tmcdowellddhs@yahoo.com>
2023-04-14 08:54:12 -0700

    pass test to return a calculator with an entered age.

--- __tests__/planet-age.test.js
+++ __tests__/planet-age.test.js
@@ -1,7 +1,7 @@
-import planetAge from './../src/planet-age.js'
+import Calculator from './../src/planet-age.js'
 
 
-describe('planetAge', () => {
+describe('Calculator', () => {
   test('should correctly create a calculator object that returns a given age in Earth Years', () => {
     const calculator = new Calculator(31)
     expect(calculator.earth).toEqual(31);
---
+++ src/planet-age.js
@@ -0,0 +1,7 @@
+
+
+export default class Calculator {
+    constructor(earthyears) {
+    this.earth = earthyears;
+    }
+}
