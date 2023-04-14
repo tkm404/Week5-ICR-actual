a5774e3db8527412c018a68dc83456e830754ce0
tkm404 <tmcdowellddhs@yahoo.com>
2023-04-14 11:20:56 -0700

    lint js for proper formatting.

--- src/planet-age.js
+++ src/planet-age.js
@@ -1,44 +1,44 @@
 
 
 export default class Calculator {
-    constructor(earthyears) {
+  constructor(earthyears) {
     this.earth = earthyears;
-    let mercuryYears = parseFloat(earthyears/.24).toFixed(2);
-    let venusYears = parseFloat(earthyears/.62).toFixed(2);
-    let marsYears = parseFloat(earthyears/1.88).toFixed(2);
-    let jupiterYears = parseFloat(earthyears/11.86).toFixed(2);
+    let mercuryYears = parseFloat(earthyears / .24).toFixed(2);
+    let venusYears = parseFloat(earthyears / .62).toFixed(2);
+    let marsYears = parseFloat(earthyears / 1.88).toFixed(2);
+    let jupiterYears = parseFloat(earthyears / 11.86).toFixed(2);
 
     this.mercury = parseFloat(mercuryYears);
     this.venus = parseFloat(venusYears);
     this.mars = parseFloat(marsYears);
     this.jupiter = parseFloat(jupiterYears);
-    }
-    yearsPassed(pastAge) {
-      let yearsPassed = this.earth - pastAge;
-      let mercuryPassed = parseFloat(yearsPassed/.24).toFixed(2);
-      let venusPassed = parseFloat(yearsPassed/.62).toFixed(2);
-      let marsPassed = parseFloat(yearsPassed/1.88).toFixed(2);
-      let jupiterPassed = parseFloat(yearsPassed/11.86).toFixed(2);
+  }
+  yearsPassed(pastAge) {
+    let yearsPassed = this.earth - pastAge;
+    let mercuryPassed = parseFloat(yearsPassed / .24).toFixed(2);
+    let venusPassed = parseFloat(yearsPassed / .62).toFixed(2);
+    let marsPassed = parseFloat(yearsPassed / 1.88).toFixed(2);
+    let jupiterPassed = parseFloat(yearsPassed / 11.86).toFixed(2);
 
-      let mercuryPast = parseFloat(mercuryPassed);
-      let venusPast = parseFloat(venusPassed);
-      let marsPast = parseFloat(marsPassed);
-      let jupiterPast = parseFloat(jupiterPassed)
-      
-      return  `${yearsPassed} Earth years have passed. ${mercuryPast} Mercury years have passed. ${venusPast} Venus years have passed. ${marsPast} Mars years have passed. ${jupiterPast} Jupiter years have passed.`
-    }
-    yearsAhead(futureAge) {
-      let yearsToGo = futureAge - this.earth;
-      let mercuryAhead = parseFloat(yearsToGo/.24).toFixed(2);
-      let venusAhead = parseFloat(yearsToGo/.62).toFixed(2);
-      let marsAhead = parseFloat(yearsToGo/1.88).toFixed(2);
-      let jupiterAhead = parseFloat(yearsToGo/11.86).toFixed(2);
+    let mercuryPast = parseFloat(mercuryPassed);
+    let venusPast = parseFloat(venusPassed);
+    let marsPast = parseFloat(marsPassed);
+    let jupiterPast = parseFloat(jupiterPassed);
 
-      let mercuryFuture = parseFloat(mercuryAhead);
-      let venusFuture = parseFloat(venusAhead);
-      let marsFuture = parseFloat(marsAhead);
-      let jupiterFuture = parseFloat(jupiterAhead);
-      
-      return  `${yearsToGo} Earth years have yet to pass. ${mercuryFuture} Mercury years have yet to pass. ${venusFuture} Venus years have yet to pass. ${marsFuture} Mars years have yet to pass. ${jupiterFuture} Jupiter years have yet to pass.` 
-    }
-};
+    return `${yearsPassed} Earth years have passed. ${mercuryPast} Mercury years have passed. ${venusPast} Venus years have passed. ${marsPast} Mars years have passed. ${jupiterPast} Jupiter years have passed.`;
+  }
+  yearsAhead(futureAge) {
+    let yearsToGo = futureAge - this.earth;
+    let mercuryAhead = parseFloat(yearsToGo / .24).toFixed(2);
+    let venusAhead = parseFloat(yearsToGo / .62).toFixed(2);
+    let marsAhead = parseFloat(yearsToGo / 1.88).toFixed(2);
+    let jupiterAhead = parseFloat(yearsToGo / 11.86).toFixed(2);
+
+    let mercuryFuture = parseFloat(mercuryAhead);
+    let venusFuture = parseFloat(venusAhead);
+    let marsFuture = parseFloat(marsAhead);
+    let jupiterFuture = parseFloat(jupiterAhead);
+
+    return `${yearsToGo} Earth years have yet to pass. ${mercuryFuture} Mercury years have yet to pass. ${venusFuture} Venus years have yet to pass. ${marsFuture} Mars years have yet to pass. ${jupiterFuture} Jupiter years have yet to pass.`;
+  }
+}
