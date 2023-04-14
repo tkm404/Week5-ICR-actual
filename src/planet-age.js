a7d696b1276afab0c7b0f536b9734ea03edf5bc4
tkm404 <tmcdowellddhs@yahoo.com>
2023-04-14 10:59:44 -0700

    pass test to determine difference in age between present and future; also account for typos.

--- src/planet-age.js
+++ src/planet-age.js
@@ -28,6 +28,17 @@ export default class Calculator {
       return  `${yearsPassed} Earth years have passed. ${mercuryPast} Mercury years have passed. ${venusPast} Venus years have passed. ${marsPast} Mars years have passed. ${jupiterPast} Jupiter years have passed.`
     }
     yearsAhead(futureAge) {
-    return "nothing yet"     
+      let yearsToGo = futureAge - this.earth;
+      let mercuryAhead = parseFloat(yearsToGo/.24).toFixed(2);
+      let venusAhead = parseFloat(yearsToGo/.62).toFixed(2);
+      let marsAhead = parseFloat(yearsToGo/1.88).toFixed(2);
+      let jupiterAhead = parseFloat(yearsToGo/11.86).toFixed(2);
+
+      let mercuryFuture = parseFloat(mercuryAhead);
+      let venusFuture = parseFloat(venusAhead);
+      let marsFuture = parseFloat(marsAhead);
+      let jupiterFuture = parseFloat(jupiterAhead);
+      
+      return  `${yearsToGo} Earth years have yet to pass. ${mercuryFuture} Mercury years have yet to pass. ${venusFuture} Venus years have yet to pass. ${marsFuture} Mars years have yet to pass. ${jupiterFuture} Jupiter years have yet to pass.` 
     }
 }
