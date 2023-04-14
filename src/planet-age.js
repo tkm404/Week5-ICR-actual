2fff64804cec15391d6d151a91a9f1906d1fde7b
tkm404 <tmcdowellddhs@yahoo.com>
2023-04-14 09:43:30 -0700

    good-fail to catch false positives in calculator.

--- src/planet-age.js
+++ src/planet-age.js
@@ -4,9 +4,9 @@ export default class Calculator {
     constructor(earthyears) {
     this.earth = earthyears;
     let mercuryYears = parseFloat(earthyears/.24).toFixed(2);
-    // let venusYears = parseFloat(earthyears/.62).toFixed(2);
-    // let marsYears 
-    // let jupiterYears
+    let venusYears = parseFloat(earthyears/.62).toFixed(2);
+    let marsYears = parseFloat(earthyears/1.88).toFixed(2);
+    let jupiterYears = parseFloat(earthyears/11.86).toFixed(2)
     this.mercury = parseFloat(mercuryYears)
     
     }
