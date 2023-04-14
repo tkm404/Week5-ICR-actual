45b2a9f27bb6d8ccb91338146a2bc5de38dd6615
tkm404 <tmcdowellddhs@yahoo.com>
2023-04-14 09:09:27 -0700

    good-fail to determine if mercury calculation is reading no false positives.

--- src/planet-age.js
+++ src/planet-age.js
@@ -3,5 +3,7 @@
 export default class Calculator {
     constructor(earthyears) {
     this.earth = earthyears;
+    // this.mercury =
+    //  Number.parseFloat(earthyears/.24).toFixed(2);
     }
 }
