e956889afffe1ab5e459e63340606ae26aa8462e
tkm404 <tmcdowellddhs@yahoo.com>
2023-04-14 09:31:29 -0700

    test pass for calculating mercury years. was returning as string, needed to parseFloat a parseFloat.

--- src/planet-age.js
+++ src/planet-age.js
@@ -3,7 +3,8 @@
 export default class Calculator {
     constructor(earthyears) {
     this.earth = earthyears;
-    // this.mercury =
-    //  Number.parseFloat(earthyears/.24).toFixed(2);
+    let mercuryYears = parseFloat(earthyears/.24).toFixed(2);
+    this.mercury = parseFloat(mercuryYears)
+    
     }
 }
