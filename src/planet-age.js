8e4bf558c3c761dd85e7bac7b379a5449dedd683
tkm404 <tmcdowellddhs@yahoo.com>
2023-04-14 09:45:32 -0700

    pass test for calculating different planetary years.

--- src/planet-age.js
+++ src/planet-age.js
@@ -6,8 +6,11 @@ export default class Calculator {
     let mercuryYears = parseFloat(earthyears/.24).toFixed(2);
     let venusYears = parseFloat(earthyears/.62).toFixed(2);
     let marsYears = parseFloat(earthyears/1.88).toFixed(2);
-    let jupiterYears = parseFloat(earthyears/11.86).toFixed(2)
-    this.mercury = parseFloat(mercuryYears)
-    
+    let jupiterYears = parseFloat(earthyears/11.86).toFixed(2);
+
+    this.mercury = parseFloat(mercuryYears);
+    this.venus = parseFloat(venusYears);
+    this.mars = parseFloat(marsYears);
+    this.jupiter = parseFloat(jupiterYears);
     }
 }
