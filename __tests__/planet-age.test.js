76d7e65cee2656e3fdad15ebce458cda875baf36
tkm404 <tmcdowellddhs@yahoo.com>
2023-04-14 09:02:55 -0700

    add test to track mercury years.

--- __tests__/planet-age.test.js
+++ __tests__/planet-age.test.js
@@ -2,8 +2,15 @@ import Calculator from './../src/planet-age.js'
 
 
 describe('Calculator', () => {
+  let tomCalculator;
+  beforeEach(() => { 
+    tomCalculator = new Calculator(31);
+  });
   test('should correctly create a calculator object that returns a given age in Earth Years', () => {
-    const calculator = new Calculator(31)
-    expect(calculator.earth).toEqual(31);
+    expect(tomCalculator.earth).toEqual(31);
+  });
+  test('should correctly return age in Mercury Years //Earth Years / .24 //', () => {
+    expect(tomCalculator.earth).toEqual(31);
+    expect(tomCalculator.mercury).toEqual(129.17);
   });
 });
